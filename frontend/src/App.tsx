238ece73f2758ffb80aee73fe811ed2841913013
Vitor Prates <prates.v@northeastern.edu>
2024-12-10 16:44:59 -0500

    home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import Profile from './pages/Profile'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
 import Home from './pages/Home'
+import Search from './pages/Search'
 import './index.css'
 import LandingPage from './pages/LandingPage'
 
@@ -11,6 +12,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/home" element={<Home />} />
+        <Route path="/search" element={<Search />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,32 @@
-export default function Home(){
-    return (
-        <div className="bg-red-500">
-            <h1>Home</h1>
-        </div>
-    )
+import "./Home.css";
+import { FaUser } from "react-icons/fa";
+import { MdOutlineVideoCameraFront } from "react-icons/md";
+import { FaSearch } from "react-icons/fa";
+
+export default function Home() {
+  return (
+    <div>
+      <head>
+        <title>Video Stream</title>
+      </head>
+      <body>
+        <header>
+          <button className="logo">
+            <MdOutlineVideoCameraFront className="icon" />
+            <span>VideoStream</span>
+          </button>
+
+          <div className="search">
+            <input type="text" className="search-bar" placeholder="Search..." />
+            <button><FaSearch className="search-icon" /></button>
+          </div>
+
+          <div className="buttons">
+            <button className="login">LOG IN</button>
+            <button className="signup">SIGN UP</button>
+          </div>
+        </header>
+      </body>
+    </div>
+  );
 }
